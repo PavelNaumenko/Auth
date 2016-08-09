2da0926f491368fb036a3b808ba00f0d96a9aab7
Pavel Naumenko <pashok1795@yandex.ru>
2016-08-09 20:14:36 +0300

    first commit

---
+++ view/lock.js
@@ -0,0 +1,47 @@
+"use strict";
+
+console.log('Hello');
+
+let lock = new Auth0Lock('mt9vUcHsEZB2hUvUfUDquC1ywjEOjnMJ', 'pashka95.eu.auth0.com');
+
+lock.on('authenticated', (authResult) => {
+
+	lock.getProfile(authResult.idToken, (error, profile) => {
+
+		if (error) {
+
+			console.log(error);
+			return;
+
+		}
+
+		fetch('http://localhost:3000/login', {
+
+			method: 'POST',
+			headers: {
+				'Content-Type': 'application/json',
+				Authorization: 'Bearer' + authResult.idToken
+			}
+
+		}).then(response => {
+
+			response.json().then(data => {
+
+				console.log(data);
+				localStorage.setItem('token', data.token);
+
+			});
+
+		});
+
+	});
+
+});
+
+document.getElementById('btn-login').addEventListener('click', () => {
+
+	lock.show();
+
+});
+
+
