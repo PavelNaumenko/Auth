def01bf2f130055477da954906299c8cbcd17f0e
pashka95 <pashok1795@yandex.ru>
2016-08-12 13:02:10 +0300

    Update entry.js

--- server/entry.js
+++ server/entry.js
@@ -3,25 +3,9 @@ import express from 'express';
 import bodyParser from 'body-parser';
 import path from 'path';
 import expressJWT from 'express-jwt';
-import mongoose from 'mongoose';
 
 const app = express();
 const port = 3000;
-
-mongoose.connect('mongodb://localhost/brockenLeg', (err) => {
-
-	if (err) {
-
-		console.log('Error: ' + err);
-
-	} else {
-
-		console.log('We are connect to DB');
-
-	}
-
-});
-
 
 /**
  * Access headers to server
@@ -44,8 +28,23 @@ app.all('/*', (req, res, next) => {
 app.use(bodyParser.json({ limit: '50mb' }));
 app.use(express.static(path.join(__dirname, '../view')));
 app.use(bodyParser.urlencoded({ limit: '50mb', extended: true }));
-// protect routes
-// app.use(expressJWT({ secret: 'secret' }).unless({ path: ('/') }));
+
+/**
+ * error handler
+ */
+
+app.use((err, req, res, next) => {
+
+	console.log('1');
+
+	if (err.name === 'UnauthorizedError') {
+
+		console.log('I am here!');
+		res.status(401).send({ message: 'invalid token...' });
+
+	}
+
+});
 
 const secret = 'T_XI2yY3H5JTbNnYjlzvbIDs9bwWXkZRSO90Eq9x1dcO7z3xJY9bGqr2Z567jg3B';
 const authenticate = expressJWT({
@@ -56,6 +55,7 @@ const authenticate = expressJWT({
 });
 
 app.use('/login', authenticate);
+app.use('/private', authenticate);
 
 /**
  * Require frontend dir public file
@@ -71,22 +71,11 @@ app.post('/login', (req, res) => {
 
 	const token = req.headers.authorization;
 
-	console.log(req.user.sub);
+	console.log(req.user);
 
-	// jwt.verify(token, 'secret', (err, decoded) => {
-	//
-	// 	if (err) {
-    //
-	// 		console.log(err);
-    //
-	// 	} else {
-    //
-	// 		console.log(decoded);
-	//
-	// 	}
-	//
-	//
-	// });
+	//Here write data to db.
+
+	res.status(200).send({ message: 'User write to db' });
 
 });
 
